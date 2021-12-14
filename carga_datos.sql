8b94982adb00de15ed21070e988bbba8e998998e
Feliciano07 <fernandoxhajon122@gmail.com>
2021-12-13 20:01:03 -0600

    carga de datos

---
+++ carga_datos.sql
@@ -0,0 +1,64 @@
+delete from Temporal
+
+CREATE TABLE Temporal(
+    banco varchar(100),
+    activo int,
+    pasivo int,
+    capital int,
+    mes int
+)
+
+
+/*
+mysql --local-infile=1 -u root -p
+SET GLOBAL local_infile=1;
+use proyecto_clase;
+
+LOAD DATA LOCAL INFILE 'C:\\control\\Datos.csv' 
+INTO TABLE Temporal
+CHARACTER SET latin1
+COLUMNS TERMINATED BY ';' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES (Banco,activo,pasivo,capital,mes);
+
+*/
+
+-- Llenado de tablas
+
+
+-- Llenado de la tabla bancos
+INSERT INTO Banco(nombre)
+SELECT DISTINCT Banco from temporal;
+
+
+
+
+
+
+-- lleando tabla maestra, activo
+INSERT INTO Banco_Rubro(cantidad,banco,rubro,mes)
+SELECT activo, bc.banco,1 as rubro, ms.mes
+from 
+temporal tmp ,
+Banco bc,
+Mes ms
+where tmp.banco = bc.nombre and tmp.mes = ms.mes
+
+
+-- llenando de pasivo
+INSERT INTO Banco_Rubro(cantidad,banco,rubro,mes)
+SELECT pasivo, bc.banco, 2 as rubro, ms.mes
+from 
+temporal tmp ,
+Banco bc,
+Mes ms
+where tmp.banco = bc.nombre and tmp.mes = ms.mes
+
+--lleando de capital
+INSERT INTO Banco_Rubro(cantidad,banco,rubro,mes)
+SELECT capital, bc.banco, 3 as rubro, ms.mes
+from 
+temporal tmp ,
+Banco bc,
+Mes ms
+where tmp.banco = bc.nombre and tmp.mes = ms.mes
---
+++ script.sql
@@ -0,0 +1,52 @@
+
+--- Tablas reales
+
+
+CREATE TABLE Banco(
+    banco int not null AUTO_INCREMENT,
+    nombre varchar(100) not null,
+    PRIMARY KEY(banco)
+);
+
+CREATE TABLE Rubro(
+    rubro int not null,
+    nombre varchar(45) not null,
+    PRIMARY KEY(rubro)
+);
+
+CREATE TABLE Mes(
+    mes int not null AUTO_INCREMENT,
+    nombre varchar(45) not null,
+    PRIMARY KEY(mes)
+);
+
+CREATE TABLE Banco_Rubro(
+    banco_rubro int not null AUTO_INCREMENT,
+    cantidad int not null,
+    banco int not null,
+    rubro int not null,
+    mes int not null,
+    PRIMARY KEY(banco_rubro),
+    CONSTRAINT fk_banco FOREIGN KEY(banco) REFERENCES Banco(banco) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_rubro FOREIGN KEY(rubro) REFERENCES Rubro(rubro) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_mes FOREIGN KEY(mes) REFERENCES Mes(mes) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO Mes(nombre) values('Enero');
+INSERT INTO Mes(nombre) values('Febrero');
+INSERT INTO Mes(nombre) values('Marzo');
+INSERT INTO Mes(nombre) values('Abril');
+INSERT INTO Mes(nombre) values('Mayo');
+INSERT INTO Mes(nombre) values('Junio');
+INSERT INTO Mes(nombre) values('Julio');
+INSERT INTO Mes(nombre) values('Agosto');
+INSERT INTO Mes(nombre) values('Septiembre');
+INSERT INTO Mes(nombre) values('Octubre');
+INSERT INTO Mes(nombre) values('Noviembre');
+INSERT INTO Mes(nombre) values('Diciembre');
+
+
+--INSERTAR RUBRO
+INSERT INTO Rubro(rubro,nombre) values(1,'Activo')
+INSERT INTO Rubro(rubro,nombre) values(2,'Pasivo')
+INSERT INTO Rubro(rubro,nombre) values(3,'Capital Contable')
